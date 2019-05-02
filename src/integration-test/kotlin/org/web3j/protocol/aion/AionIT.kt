347d83b52bffd948c58de73c32718572d10ecb32
Xavier <xavier@blk.io>
2019-05-02 11:39:18 +0100

    WIP Deploy Solidity contract using a web3j contract wrapper

--- src/integration-test/kotlin/org/web3j/protocol/aion/AionIT.kt
+++ src/integration-test/kotlin/org/web3j/protocol/aion/AionIT.kt
@@ -1,21 +1,39 @@
 package org.web3j.protocol.aion
 
+import net.i2p.crypto.eddsa.EdDSASecurityProvider
 import org.assertj.core.api.Assertions.assertThat
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.openssl.PEMKeyPair
+import org.bouncycastle.openssl.PEMParser
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import org.web3j.crypto.Credentials
+import org.web3j.crypto.ECKeyPair
 import org.web3j.protocol.core.DefaultBlockParameterName.PENDING
 import org.web3j.protocol.http.HttpService
 import org.web3j.testcontract.TestContract
-import org.web3j.tx.ClientTransactionManager
-import org.web3j.tx.TransactionManager
 import org.web3j.tx.gas.DefaultGasProvider
-import java.math.BigInteger
+import java.io.File
+import java.io.IOException
+import java.io.StringReader
+import java.security.Key
+import java.security.KeyPair
+import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.Security
 
 @Testcontainers
 class AionIT {
+
+    init {
+        Security.addProvider(BouncyCastleProvider())
+        Security.addProvider(EdDSASecurityProvider())
+    }
 
     @Test
     fun testGetUncleCountByBlockHash() {
@@ -32,27 +50,65 @@ class AionIT {
         val balance = aion.ethGetBalance(ADDRESS, PENDING).send()
 
         assertThat(balance.error).isNull()
-        assertThat(balance.balance).isEqualTo(BigInteger("465934586660000000000000000"))
+        assertThat(balance.balance).isZero()
     }
 
     @Test
     internal fun testContractDeploy() {
-        TestContract.deploy(aion, manager, DefaultGasProvider(), "").send()
+        val file = File(javaClass.classLoader.getResource(ACCOUNT).file)
+
+//        aion.personalUnlockAccount(ACCOUNT, "410n")
+        val keys = getKeys(file)
+
+//        val data = FileUtils.readFileToString(file, Charsets.UTF_8)
+//        val ecKeyPair = ECKeyPair.create(getKeyPair(data))
+//        val credentials = Credentials.create(ecKeyPair)
+//        val credentials = WalletUtils.loadCredentials("410n", file)
+        val keyPair = KeyPair(keys[0] as PublicKey, keys[1] as PrivateKey)
+        val credentials = Credentials.create(ECKeyPair.create(keyPair))
+
+        TestContract.deploy(aion, credentials, DefaultGasProvider(), "").send()
     }
 
     companion object {
 
-        private const val ADDRESS = "0x0000000000000000000000000000000000000000000000000000000000000000"
+        private const val ADDRESS = "0xa0d5c14a9a2f84a1a8b20fbc329f27e8cb2d2dc0752bb4411b9cd77814355ce6"
+        private const val ACCOUNT = "a0d5c14a9a2f84a1a8b20fbc329f27e8cb2d2dc0752bb4411b9cd77814355ce6"
 
         private lateinit var aion: Aion
-        private lateinit var manager: TransactionManager
 
         @BeforeAll
         @JvmStatic
         fun initClient() {
             val url = "http://${AION.containerIpAddress}:${AION.getMappedPort(8545)}/"
             aion = Aion.build(HttpService(url))
-            manager = ClientTransactionManager(aion, ADDRESS)
+        }
+
+        private fun getKeys(file: File): List<Key> {
+            val keyStore = KeyStore.getInstance("PKCS12")
+            val password = "410n".toCharArray()
+
+            keyStore.load(file.inputStream(), password)
+
+            return keyStore.aliases().toList().map {
+                keyStore.getKey(it, password)
+            }
+        }
+
+        private fun getKeyPair(data: String): KeyPair {
+            PEMParser(StringReader(data)).use {
+
+                val pemKeyPair = when (val pemPair = it.readObject()) {
+                    is PEMKeyPair -> pemPair
+                    else -> throw IOException("Unexpected PEM object from $pemPair")
+                }
+
+                val converter = JcaPEMKeyConverter().setProvider(EdDSASecurityProvider.PROVIDER_NAME)
+                val privateKey = converter.getPrivateKey(pemKeyPair.privateKeyInfo)
+                val publicKey = converter.getPublicKey(pemKeyPair.publicKeyInfo)
+
+                return KeyPair(publicKey, privateKey)
+            }
         }
 
         @Container
