861b2b388cdef40b77a171cf7af15eb4ee8e8514
Xavier <xavier@blk.io>
2019-05-08 14:37:04 +0100

    Add unit tests for JSON RPC deprecated and unsupported functions

--- src/test/kotlin/org/web3j/protocol/aion/AionTest.kt
+++ src/test/kotlin/org/web3j/protocol/aion/AionTest.kt
@@ -1,8 +1,17 @@
 package org.web3j.protocol.aion
 
+import assertk.Assert
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNull
 import org.junit.Test
 import org.web3j.protocol.RequestTester
+import org.web3j.protocol.core.DefaultBlockParameterName.LATEST
 import org.web3j.protocol.core.DefaultBlockParameterName.PENDING
+import org.web3j.protocol.core.Response
+import org.web3j.protocol.core.RpcErrors
+import org.web3j.protocol.core.methods.request.ShhFilter
+import org.web3j.protocol.core.methods.request.ShhPost
 import org.web3j.protocol.core.methods.request.Transaction
 import org.web3j.protocol.http.HttpService
 import java.math.BigInteger.ONE
@@ -18,53 +27,149 @@ class AionTest : RequestTester() {
     }
 
     @Test
+    fun testEthGetUncleCountByBlockHash() {
+        assertThat(aion.ethGetUncleCountByBlockHash(HASH).send()).isError()
+    }
+
+    @Test
+    fun testEthGetUncleCountByBlockNumber() {
+        assertThat(aion.ethGetUncleCountByBlockNumber(LATEST).send()).isError()
+    }
+
+    @Test
+    fun testEthGetUncleByBlockHashAndIndex() {
+        assertThat(aion.ethGetUncleByBlockHashAndIndex(HASH, ONE).send()).isError()
+    }
+
+    @Test
+    fun testEthGetUncleByBlockNumberAndIndex() {
+        assertThat(aion.ethGetUncleByBlockNumberAndIndex(LATEST, ONE).send()).isError()
+    }
+
+    @Test
+    fun testEthCompileLLL() {
+        assertThat(aion.ethCompileLLL("").send()).isError()
+    }
+
+    @Test
+    fun testEthCompileSerpent() {
+        assertThat(aion.ethCompileSerpent("").send()).isError()
+    }
+
+    @Test
+    fun testEthGetWork() {
+        assertThat(aion.ethGetWork().send()).isError()
+    }
+
+    @Test
+    fun testEthSubmitWork() {
+        assertThat(aion.ethSubmitWork("", "", "").send()).isError()
+    }
+
+    @Test
+    fun testShhPost() {
+        assertThat(aion.shhPost(ShhPost(mutableListOf<String>(), "", ONE, ONE)).send()).isError()
+    }
+
+    @Test
+    fun testShhVersion() {
+        assertThat(aion.shhVersion().send()).isError()
+    }
+
+    @Test
+    fun testShhNewIdentity() {
+        assertThat(aion.shhNewIdentity().send()).isError()
+    }
+
+    @Test
+    fun testShhHasIdentity() {
+        assertThat(aion.shhHasIdentity(ADDRESS).send()).isError()
+    }
+
+    @Test
+    fun testShhNewGroup() {
+        assertThat(aion.shhNewGroup().send()).isError()
+    }
+
+    @Test
+    fun testShhAddToGroup() {
+        assertThat(aion.shhAddToGroup("").send()).isError()
+    }
+
+    @Test
+    fun testShhNewFilter() {
+        assertThat(aion.shhNewFilter(ShhFilter("")).send()).isError()
+    }
+
+    @Test
+    fun testShhUninstallFilter() {
+        assertThat(aion.shhUninstallFilter(ONE).send()).isError()
+    }
+
+    @Test
+    fun testShhGetFilterChanges() {
+        assertThat(aion.shhGetFilterChanges(ONE).send()).isError()
+    }
+
+    @Test
+    fun testShhGetMessages() {
+        assertThat(aion.shhGetMessages(ONE).send()).isError()
+    }
+
+    @Test
+    fun testDbPutString() {
+        assertThat(aion.dbPutString("", "", "").send()).isError()
+    }
+
+    @Test
+    fun testDbGetString() {
+        assertThat(aion.dbGetString("", "").send()).isError()
+    }
+
+    @Test
+    fun testDbPutHex() {
+        assertThat(aion.dbPutHex("", "", "").send()).isError()
+    }
+
+    @Test
+    fun testDbGetHex() {
+        assertThat(aion.dbGetHex("", "").send()).isError()
+    }
+
+    @Test(expected = AionProtocolException::class)
     fun testGetBalance() {
-        val address = "0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2"
-        aion.ethGetBalance(address, PENDING).send()
-
-        verifyResult("{\"method\":\"eth_getBalance\",\"params\":[\"0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2\"],\"id\":0}")
+        aion.ethGetBalance(ADDRESS, PENDING).send()
     }
 
-    @Test
+    @Test(expected = AionProtocolException::class)
     fun testGetCode() {
-        val address = "0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2"
-        aion.ethGetCode(address, PENDING).send()
-
-        verifyResult("{\"method\":\"eth_getCode\",\"params\":[\"0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2\"],\"id\":0}")
+        aion.ethGetCode(ADDRESS, PENDING).send()
     }
 
-    @Test
+    @Test(expected = AionProtocolException::class)
     fun testGetTransactionCount() {
-        val address = "0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2"
-        aion.ethGetTransactionCount(address, PENDING).send()
-
-        verifyResult("{\"method\":\"eth_getTransactionCount\",\"params\":[\"0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2\"],\"id\":0}")
+        aion.ethGetTransactionCount(ADDRESS, PENDING).send()
     }
 
-    @Test
+    @Test(expected = AionProtocolException::class)
     fun testGetStorageAt() {
-        val address = "0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2"
-        aion.ethGetStorageAt(address, ONE, PENDING).send()
-
-        verifyResult("{\"method\":\"eth_getStorageAt\",\"params\":[\"0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2\",\"0x1\"],\"id\":0}")
+        aion.ethGetStorageAt(ADDRESS, ONE, PENDING).send()
     }
 
-    @Test
+    @Test(expected = AionProtocolException::class)
     fun testCall() {
-        val from = "0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2"
-        val to = "0xa0c6ed9486e9137802d0acdcd9a0499241872f648b51a5ab49a534a0d440f62c"
-
-        val data = """f0a147ad
-            a069071db28d1f8676766a82272e433e
-            46bd80773e69bbbe92dccf2306e042b1
-            000000000000000104295b07fb4fa600
-            00000000000000000000000000000040
-            00000000000000000000000000000000
-        """.trimIndent()
-
-        val transaction = Transaction(from, ZERO, ONE, ONE, to, TEN, data)
+        val transaction = Transaction(ADDRESS, ZERO, ONE, ONE, ADDRESS, TEN, "")
         aion.ethCall(transaction, PENDING).send()
+    }
 
-        verifyResult("{\"method\":\"eth_call\",\"params\":[{\"from\":\"0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2\",\"to\":\"0xa0c6ed9486e9137802d0acdcd9a0499241872f648b51a5ab49a534a0d440f62c\",\"gas\":\"0x1\",\"gasPrice\":\"0x1\",\"value\":\"0xa\",\"data\":\"0xf0a147ad\\n            a069071db28d1f8676766a82272e433e\\n            46bd80773e69bbbe92dccf2306e042b1\\n            000000000000000104295b07fb4fa600\\n            00000000000000000000000000000040\\n            00000000000000000000000000000000\",\"nonce\":\"0x0\"}],\"id\":<generatedValue>}")
+    private fun <T : Response<*>> Assert<T>.isError() = given { response ->
+        assertThat(response.error.code).isEqualTo(RpcErrors.INVALID_REQUEST)
+        assertThat(response.error.message).isEqualTo("Method not found")
+        assertThat(response.result).isNull()
+    }
+
+    companion object {
+        const val ADDRESS = "0xa0b2e70f995df21e7988d645f1fdbaa94324f82b1d162b9b45cdbef7b5b51bd2"
+        const val HASH = "0x34e9d4af6d2bace6a54b6039843e55bdc1d4ff425cd8e8c8f3fa96ca89f58a53"
     }
 }
