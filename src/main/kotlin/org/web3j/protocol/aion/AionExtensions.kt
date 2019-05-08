496bed4dfa0aeaf11157dcbcd7af70bfe4093b34
Xavier <xavier@blk.io>
2019-05-08 15:19:04 +0100

    Add Aion nomenclature for gas as extension functions

--- src/main/kotlin/org/web3j/protocol/aion/AionExtensions.kt
+++ src/main/kotlin/org/web3j/protocol/aion/AionExtensions.kt
@@ -1,6 +1,13 @@
 package org.web3j.protocol.aion
 
 import org.web3j.abi.datatypes.Address
+import org.web3j.protocol.core.methods.response.Transaction
+import org.web3j.protocol.core.methods.response.TransactionReceipt
+
+fun Transaction.getNrg() = gas
+fun Transaction.getNrgPrice() = gasPrice
+
+fun TransactionReceipt.getNrgUsed() = gasUsed
 
 fun Address.toAion(): avm.Address {
     return avm.Address(toString().toByteArray())
