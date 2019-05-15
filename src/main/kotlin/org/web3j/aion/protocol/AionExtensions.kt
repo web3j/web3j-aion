b201d519a1733c5a33140df42b1ecc228a3f5c74
Xavier <xavier@blk.io>
2019-05-15 10:31:25 +0100

    Add transaction NRG extensions as properties instead of methods

--- src/main/kotlin/org/web3j/aion/protocol/AionExtensions.kt
+++ src/main/kotlin/org/web3j/aion/protocol/AionExtensions.kt
@@ -3,11 +3,50 @@ package org.web3j.aion.protocol
 import org.web3j.abi.datatypes.Address
 import org.web3j.protocol.core.methods.response.Transaction
 import org.web3j.protocol.core.methods.response.TransactionReceipt
+import org.web3j.utils.Numeric
+import java.math.BigInteger
 
-fun Transaction.getNrg() = gas
-fun Transaction.getNrgPrice() = gasPrice
+val org.web3j.protocol.core.methods.request.Transaction.nrg: String
+    get() = gas
 
-fun TransactionReceipt.getNrgUsed() = gasUsed
+val org.web3j.protocol.core.methods.request.Transaction.nrgPrice: String
+    get() = gasPrice
+
+var Transaction.nrg: BigInteger
+    get() = gas
+    set(value) {
+        setGas(Numeric.encodeQuantity(value))
+    }
+
+var Transaction.nrgRaw: String
+    get() = gasRaw
+    set(value) {
+        setGas(value)
+    }
+
+var Transaction.nrgPrice: BigInteger
+    get() = gasPrice
+    set(value) {
+        setGasPrice(Numeric.encodeQuantity(value))
+    }
+
+var Transaction.nrgPriceRaw: String
+    get() = gasPriceRaw
+    set(value) {
+        setGasPrice(value)
+    }
+
+var TransactionReceipt.nrgUsed: BigInteger
+    get() = gasUsed
+    set(value) {
+        setGasUsed(Numeric.encodeQuantity(value))
+    }
+
+var TransactionReceipt.nrgRaw: String
+    get() = gasUsedRaw
+    set(value) {
+        setGasUsed(value)
+    }
 
 fun Address.toAion(): avm.Address {
     return avm.Address(toString().toByteArray())
