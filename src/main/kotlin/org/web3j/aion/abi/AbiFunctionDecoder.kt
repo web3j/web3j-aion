648cc2cf7a68da19fe376447dadadd06daf4f1aa
Xavier <xavier@blk.io>
2019-06-05 15:42:08 +0100

    Implement FVM numeric type decoding

--- src/main/kotlin/org/web3j/aion/abi/AbiFunctionDecoder.kt
+++ src/main/kotlin/org/web3j/aion/abi/AbiFunctionDecoder.kt
@@ -12,7 +12,23 @@ import org.web3j.abi.datatypes.DynamicArray
 import org.web3j.abi.datatypes.IntType
 import org.web3j.abi.datatypes.StaticArray
 import org.web3j.abi.datatypes.Type
+import org.web3j.abi.datatypes.Uint
 import org.web3j.abi.datatypes.Utf8String
+import org.web3j.abi.datatypes.generated.Int16
+import org.web3j.abi.datatypes.generated.Int24
+import org.web3j.abi.datatypes.generated.Int32
+import org.web3j.abi.datatypes.generated.Int40
+import org.web3j.abi.datatypes.generated.Int48
+import org.web3j.abi.datatypes.generated.Int56
+import org.web3j.abi.datatypes.generated.Int64
+import org.web3j.abi.datatypes.generated.Int8
+import org.web3j.abi.datatypes.generated.Uint16
+import org.web3j.abi.datatypes.generated.Uint24
+import org.web3j.abi.datatypes.generated.Uint32
+import org.web3j.abi.datatypes.generated.Uint40
+import org.web3j.abi.datatypes.generated.Uint48
+import org.web3j.abi.datatypes.generated.Uint56
+import org.web3j.abi.datatypes.generated.Uint8
 import org.web3j.abi.datatypes.primitive.Byte
 import org.web3j.abi.datatypes.primitive.Char
 import org.web3j.abi.datatypes.primitive.Double
@@ -26,6 +42,21 @@ import java.math.BigInteger
 
 internal object AbiFunctionDecoder : FunctionReturnDecoder() {
 
+    /**
+     * Regex to match the bit size of an [org.web3j.abi.datatypes.Int].
+     */
+    private val INT_REGEX = "^Int(\\d*)\$".toRegex()
+
+    /**
+     * Regex to match the bit size of an unsigned [Uint].
+     */
+    private val UINT_REGEX = "^Uint(\\d*)\$".toRegex()
+
+    /**
+     * Regex to match the bit size of an unsigned [Uint].
+     */
+    private val BYTES_REGEX = "^Bytes(\\d*)\$".toRegex()
+
     override fun decodeFunctionResult(
         rawInput: String,
         outputParameters: List<TypeReference<Type<*>>>
@@ -37,22 +68,23 @@ internal object AbiFunctionDecoder : FunctionReturnDecoder() {
     }
 
     private fun decodeResult(typeReference: TypeReference<Type<*>>, decoder: ABIDecoder): Type<*> {
+        val type: Class<Type<*>> = typeReference.classType
         return decoder.run {
-            when (typeReference.classType) {
-                Address::class.java -> decodeOneAddress().toWeb3j()
-                Char::class.java -> decodeOneCharacter().toWeb3j()
-                Byte::class.java -> decodeOneByte().toWeb3j()
-                Short::class.java -> decodeOneShort().toWeb3j()
-                Int::class.java -> decodeOneInteger().toWeb3j()
-                Float::class.java -> decodeOneFloat().toWeb3j()
-                Long::class.java -> decodeOneLong().toWeb3j()
-                Double::class.java -> decodeOneDouble().toWeb3j()
-                Utf8String::class.java -> decodeOneString().toWeb3j()
-                Bool::class.java -> decodeOneBoolean().toWeb3j()
-                BytesType::class.java -> decodeBytesType(typeReference, decoder)
-                IntType::class.java -> decodeIntType(typeReference, decoder)
-                DynamicArray::class.java -> decodeDynamicArray(typeReference, decoder)
-                StaticArray::class.java -> decodeStaticArray(typeReference, decoder)
+            when {
+                Address::class.java.isAssignableFrom(type) -> decodeOneAddress().toWeb3j()
+                Char::class.java.isAssignableFrom(type) -> decodeOneCharacter().toWeb3j()
+                Byte::class.java.isAssignableFrom(type) -> decodeOneByte().toWeb3j()
+                Short::class.java.isAssignableFrom(type) -> decodeOneShort().toWeb3j()
+                Int::class.java.isAssignableFrom(type) -> decodeOneInteger().toWeb3j()
+                Float::class.java.isAssignableFrom(type) -> decodeOneFloat().toWeb3j()
+                Long::class.java.isAssignableFrom(type) -> decodeOneLong().toWeb3j()
+                Double::class.java.isAssignableFrom(type) -> decodeOneDouble().toWeb3j()
+                Utf8String::class.java.isAssignableFrom(type) -> decodeOneString().toWeb3j()
+                Bool::class.java.isAssignableFrom(type) -> decodeOneBoolean().toWeb3j()
+                BytesType::class.java.isAssignableFrom(type) -> decodeBytesType(type, decoder)
+                IntType::class.java.isAssignableFrom(type) -> decodeIntType(type, decoder)
+                DynamicArray::class.java.isAssignableFrom(type) -> decodeDynamicArray(typeReference, decoder)
+                StaticArray::class.java.isAssignableFrom(type) -> decodeStaticArray(typeReference, decoder)
                 else -> throw ABIException("Unsupported ABI type")
             }
         }
@@ -65,11 +97,59 @@ internal object AbiFunctionDecoder : FunctionReturnDecoder() {
         TODO("Solidity events not implemented")
     }
 
-    private fun decodeIntType(typeReference: TypeReference<Type<*>>, decoder: ABIDecoder): IntType {
-        TODO("not implemented")
+    private fun decodeIntType(type: Class<Type<*>>, decoder: ABIDecoder): IntType {
+        return when {
+            org.web3j.abi.datatypes.Int::class.java.isAssignableFrom(type) ->
+                when (type.bitSize) {
+                    8 -> Int8(decoder.decodeOneByte().toLong())
+                    16 -> Int16(decoder.decodeOneShort().toLong())
+                    24 -> Int24(decoder.decodeOneInteger().toLong())
+                    32 -> Int32(decoder.decodeOneInteger().toLong())
+                    40 -> Int40(decoder.decodeOneLong())
+                    48 -> Int48(decoder.decodeOneLong())
+                    56 -> Int56(decoder.decodeOneLong())
+                    64 -> Int64(decoder.decodeOneLong())
+                    else -> newNumericInstance(type.bitSize, decoder, false)
+                }
+            Uint::class.java.isAssignableFrom(type) ->
+                when (type.bitSize) {
+                    8 -> Uint8(decoder.decodeOneShort().toLong())
+                    16 -> Uint16(decoder.decodeOneInteger().toLong())
+                    24 -> Uint24(decoder.decodeOneInteger().toLong())
+                    32 -> Uint32(decoder.decodeOneLong())
+                    40 -> Uint40(decoder.decodeOneLong())
+                    48 -> Uint48(decoder.decodeOneLong())
+                    56 -> Uint56(decoder.decodeOneLong())
+                    else -> newNumericInstance(type.bitSize, decoder, true)
+                }
+            else -> throw ABIException("Unknown type ${javaClass.canonicalName}")
+        }
     }
 
-    private fun decodeBytesType(typeReference: TypeReference<Type<*>>, decoder: ABIDecoder): BytesType {
+    private val Class<Type<*>>.bitSize: kotlin.Int?
+        get() = when {
+            INT_REGEX.matches(simpleName) -> bitSize(simpleName, INT_REGEX)
+            UINT_REGEX.matches(simpleName) -> bitSize(simpleName, UINT_REGEX)
+            BYTES_REGEX.matches(simpleName) -> bitSize(simpleName, BYTES_REGEX)
+            else -> throw ABIException("Unknown type ${javaClass.canonicalName}")
+        }
+
+    private fun bitSize(input: String, regex: Regex): kotlin.Int? {
+        return regex.matchEntire(input)?.groupValues?.get(1)?.toInt()
+    }
+
+    private fun newNumericInstance(bitSize: kotlin.Int?, decoder: ABIDecoder, unsigned: Boolean): IntType {
+
+        val className = "${if (unsigned) "Int" else "Uint"}${bitSize ?: ""}"
+        val packageName = "org.web3j.abi.datatypes${if (bitSize != null) ".generated" else ""}"
+
+        val constructor = Class.forName("$packageName$className")
+            .getDeclaredConstructor(BigInteger::class.java)
+
+        return constructor.newInstance(BigInteger(decoder.decodeOneByteArray())) as IntType
+    }
+
+    private fun decodeBytesType(type: Class<Type<*>>, decoder: ABIDecoder): BytesType {
         TODO("not implemented")
     }
 
@@ -126,7 +206,7 @@ internal object AbiFunctionDecoder : FunctionReturnDecoder() {
     private fun <T> T.toWeb3j(): Type<*> {
         return when (this) {
             is avm.Address -> Address(BigInteger(unwrap()))
-            is kotlin.Boolean -> Bool(this)
+            is Boolean -> Bool(this)
             is kotlin.Char -> Char(this)
             is kotlin.Byte -> Byte(this)
             is kotlin.Short -> Short(this)
