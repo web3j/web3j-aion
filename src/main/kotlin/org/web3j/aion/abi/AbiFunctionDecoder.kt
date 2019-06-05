30c6a90321c795eb54f9de9c2ab467ce8e6bf395
Xavier <xavier@blk.io>
2019-06-05 13:14:07 +0100

    Implement AVM decoding and unit tests

--- src/main/kotlin/org/web3j/aion/abi/AbiFunctionDecoder.kt
+++ src/main/kotlin/org/web3j/aion/abi/AbiFunctionDecoder.kt
@@ -1,59 +1,60 @@
 package org.web3j.aion.abi
 
 // FIXME Remove core dependency
-import org.aion.avm.core.util.ABIUtil
+import org.aion.avm.userlib.abi.ABIDecoder
 import org.aion.avm.userlib.abi.ABIException
 import org.web3j.abi.FunctionReturnDecoder
 import org.web3j.abi.TypeReference
 import org.web3j.abi.datatypes.Address
-import org.web3j.abi.datatypes.Array
+import org.web3j.abi.datatypes.Bool
+import org.web3j.abi.datatypes.BytesType
+import org.web3j.abi.datatypes.DynamicArray
 import org.web3j.abi.datatypes.IntType
+import org.web3j.abi.datatypes.StaticArray
 import org.web3j.abi.datatypes.Type
 import org.web3j.abi.datatypes.Utf8String
-import org.web3j.abi.datatypes.primitive.Boolean
 import org.web3j.abi.datatypes.primitive.Byte
 import org.web3j.abi.datatypes.primitive.Char
 import org.web3j.abi.datatypes.primitive.Double
 import org.web3j.abi.datatypes.primitive.Float
+import org.web3j.abi.datatypes.primitive.Int
 import org.web3j.abi.datatypes.primitive.Long
 import org.web3j.abi.datatypes.primitive.Short
 import org.web3j.utils.Numeric
+import java.lang.reflect.ParameterizedType
 import java.math.BigInteger
 
-object AbiFunctionDecoder : FunctionReturnDecoder() {
+internal object AbiFunctionDecoder : FunctionReturnDecoder() {
 
     override fun decodeFunctionResult(
         rawInput: String,
         outputParameters: List<TypeReference<Type<*>>>
     ): List<Type<*>> {
-
-        val bytes = Numeric.hexStringToByteArray(rawInput)
-        var range = 0..sizeOf(Utf8String::class.java, bytes, 0) + 2
-
-        // Decode and ignore function name
-        ABIUtil.decodeOneObject(bytes.sliceArray(range))
-
-        val result = mutableListOf<Type<*>>()
-
-        for (param in outputParameters) {
-            val size = sizeOf(param.classType, bytes, range.start)
-            range = range.endInclusive + 1..range.endInclusive + size + 1
-
-            ABIUtil.decodeOneObject(bytes.sliceArray(range))
-                .toAionValue(param.classType).let { result.add(it) }
+        return ABIDecoder(Numeric.hexStringToByteArray(rawInput)).run {
+            decodeMethodName() // FIXME Remove as method name won't be returned
+            outputParameters.map { decodeResult(it, this) }
         }
-
-        return result
     }
 
-    private fun <T : Type<*>> sizeOf(type: Class<T>, data: ByteArray, index: Int): Int {
-        return when (type) {
-            Boolean::class.java, Byte::class.java -> 1
-            Char::class.java, Short::class.java -> 2
-            Int::class.java, Float::class.java -> 4
-            Long::class.java, Double::class.java -> 8
-            Utf8String::class.java, Array::class.java -> data[index + 1] + data[index + 2]
-            else -> throw ABIException("Unsupported ABI type")
+    private fun decodeResult(typeReference: TypeReference<Type<*>>, decoder: ABIDecoder): Type<*> {
+        return decoder.run {
+            when (typeReference.classType) {
+                Address::class.java -> decodeOneAddress().toWeb3j()
+                Char::class.java -> decodeOneCharacter().toWeb3j()
+                Byte::class.java -> decodeOneByte().toWeb3j()
+                Short::class.java -> decodeOneShort().toWeb3j()
+                Int::class.java -> decodeOneInteger().toWeb3j()
+                Float::class.java -> decodeOneFloat().toWeb3j()
+                Long::class.java -> decodeOneLong().toWeb3j()
+                Double::class.java -> decodeOneDouble().toWeb3j()
+                Utf8String::class.java -> decodeOneString().toWeb3j()
+                Bool::class.java -> decodeOneBoolean().toWeb3j()
+                BytesType::class.java -> decodeBytesType(typeReference, decoder)
+                IntType::class.java -> decodeIntType(typeReference, decoder)
+                DynamicArray::class.java -> decodeDynamicArray(typeReference, decoder)
+                StaticArray::class.java -> decodeStaticArray(typeReference, decoder)
+                else -> throw ABIException("Unsupported ABI type")
+            }
         }
     }
 
@@ -61,46 +62,89 @@ object AbiFunctionDecoder : FunctionReturnDecoder() {
         rawInput: String,
         typeReference: TypeReference<T>
     ): Type<*> {
-        TODO("Events not implemented")
+        TODO("Solidity events not implemented")
+    }
+
+    private fun decodeIntType(typeReference: TypeReference<Type<*>>, decoder: ABIDecoder): IntType {
+        TODO("not implemented")
+    }
+
+    private fun decodeBytesType(typeReference: TypeReference<Type<*>>, decoder: ABIDecoder): BytesType {
+        TODO("not implemented")
     }
 
     @Suppress("UNCHECKED_CAST")
-    private fun <T : Type<*>> Any.toAionValue(classType: Class<T>): T {
+    private fun decodeDynamicArray(typeReference: TypeReference<*>, decoder: ABIDecoder): DynamicArray<Type<*>> {
+        val type: Class<Type<*>> = getParameterizedType(typeReference)
+
+        return decoder.run {
+            when (type) {
+                Address::class.java -> DynamicArray(
+                    Address::class.java,
+                    decodeOneAddressArray().map { it.toWeb3j() } as List<Address>)
+                Utf8String::class.java -> DynamicArray(
+                    Utf8String::class.java,
+                    decodeOneStringArray().map { it.toWeb3j() } as List<Utf8String>)
+                Bool::class.java -> DynamicArray(
+                    Bool::class.java,
+                    decodeOneBooleanArray().map { it.toWeb3j() } as List<Bool>)
+                Char::class.java -> DynamicArray(
+                    Char::class.java,
+                    decodeOneCharacterArray().map { it.toWeb3j() } as List<Char>)
+                Byte::class.java -> DynamicArray(
+                    Byte::class.java,
+                    decodeOneByteArray().map { it.toWeb3j() } as List<Byte>)
+                Short::class.java -> DynamicArray(
+                    Short::class.java,
+                    decodeOneShortArray().map { it.toWeb3j() } as List<Short>)
+                Int::class.java -> DynamicArray(
+                    Int::class.java,
+                    decodeOneIntegerArray().map { it.toWeb3j() } as List<Int>)
+                Float::class.java -> DynamicArray(
+                    Float::class.java,
+                    decodeOneFloatArray().map { it.toWeb3j() } as List<Float>)
+                Long::class.java -> DynamicArray(
+                    Long::class.java,
+                    decodeOneLongArray().map { it.toWeb3j() } as List<Long>)
+                Double::class.java -> DynamicArray(
+                    Double::class.java,
+                    decodeOneDoubleArray().map { it.toWeb3j() } as List<Double>)
+                BytesType::class.java -> TODO()
+                IntType::class.java -> TODO()
+                DynamicArray::class.java -> TODO("2D arrays not implemented")
+                StaticArray::class.java -> TODO("2D arrays not implemented")
+                else -> throw ABIException("Unsupported ABI type")
+            } as DynamicArray<Type<*>>
+        }
+    }
+
+    private fun decodeStaticArray(typeReference: TypeReference<Type<*>>, decoder: ABIDecoder): StaticArray<Type<*>> {
+        TODO("not implemented")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> T.toWeb3j(): Type<*> {
         return when (this) {
-            is kotlin.Boolean -> Boolean(this)
-            is kotlin.Byte -> Byte(this)
+            is avm.Address -> Address(BigInteger(unwrap()))
+            is kotlin.Boolean -> Bool(this)
             is kotlin.Char -> Char(this)
-            is kotlin.Long -> Long(this)
+            is kotlin.Byte -> Byte(this)
             is kotlin.Short -> Short(this)
+            is kotlin.Int -> Int(this)
             is kotlin.Float -> Float(this)
+            is kotlin.Long -> Long(this)
             is kotlin.Double -> Double(this)
-            is String -> when (classType) {
-                Utf8String::class -> Utf8String(this)
-                Address::class -> Address(ADDRESS_BIT_LENGTH, this)
-                else -> throw ABIException(javaClass.canonicalName)
-            }
-            is ByteArray -> when (classType) {
-                IntType::class -> {
-                    // Conversion from Aion byte[] to Solidity numeric value
-                    val constructor = classType.getDeclaredConstructor(BigInteger::class.java)
-                    constructor.newInstance(BigInteger(this))
-                }
-                Array::class -> {
-                    // Conversion from Aion byte[] to Solidity static or dynamic array
-                    val constructor = classType.getDeclaredConstructor(ByteArray::class.java)
-                    constructor.newInstance(this)
-                }
-                else -> throw ABIException(javaClass.canonicalName)
-            }
-            is kotlin.Array<*> -> {
-                // Conversion from Aion array to Solidity static or dynamic array
-                val constructor = classType.getDeclaredConstructor(Class::class.java, kotlin.Array<Any>::class.java)
-                constructor.newInstance(
-                    classType,
-                    *this.map { it!!.toAionValue(Utf8String::class.java) }.toTypedArray()
-                )
-            }
-            else -> throw ABIException(javaClass.canonicalName)
-        } as T
+            is String -> Utf8String(this)
+            else -> throw ABIException("Unsupported ABI type")
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T : Type<*>> getParameterizedType(
+        typeReference: TypeReference<*>
+    ): Class<T> {
+        val typeArguments = (typeReference.type as ParameterizedType).actualTypeArguments
+        val parameterizedTypeName = typeArguments[0].typeName
+        return Class.forName(parameterizedTypeName) as Class<T>
     }
 }
--- src/test/kotlin/org/web3j/aion/abi/AbiFunctionDecoderAvmTest.kt
+++ src/test/kotlin/org/web3j/aion/abi/AbiFunctionDecoderAvmTest.kt
@@ -5,8 +5,15 @@ import assertk.assertions.isEqualTo
 import org.junit.Test
 import org.web3j.abi.FunctionReturnDecoder.decode
 import org.web3j.abi.TypeReference
+import org.web3j.abi.datatypes.DynamicArray
 import org.web3j.abi.datatypes.Function
-import org.web3j.abi.datatypes.primitive.Boolean
+import org.web3j.abi.datatypes.primitive.Byte
+import org.web3j.abi.datatypes.primitive.Char
+import org.web3j.abi.datatypes.primitive.Double
+import org.web3j.abi.datatypes.primitive.Float
+import org.web3j.abi.datatypes.primitive.Int
+import org.web3j.abi.datatypes.primitive.Long
+import org.web3j.abi.datatypes.primitive.Short
 
 /**
  * TODO 2 dimensional array tests.
@@ -14,191 +21,206 @@ import org.web3j.abi.datatypes.primitive.Boolean
 class AbiFunctionDecoderAvmTest {
 
     @Test
-    fun `decode boolean`() {
-        val function = Function("test", listOf(), listOf(object : TypeReference<Boolean>() {}))
-        assertThat(decode("0x210004746573740201", function.outputParameters))
-            .isEqualTo(listOf(Boolean(true)))
+    fun `decode char`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<Char>() {}))
+        assertThat(decode("0x21000474657374030061", function.outputParameters))
+            .isEqualTo(listOf(Char('a')))
     }
-//
-//    @Test
-//    fun `decode char`() {
-//        assertThat(decode(Function("test", listOf(Char('a')), listOf())))
-//            .isEqualTo("0x21000474657374030061")
-//    }
-//
-//    @Test
-//    fun `decode byte`() {
-//        assertThat(decode(Function("test", listOf(Byte(kotlin.Byte.MIN_VALUE)), listOf())))
-//            .isEqualTo("0x210004746573740180")
-//
-//        assertThat(decode(Function("test", listOf(Byte(kotlin.Byte.MAX_VALUE)), listOf())))
-//            .isEqualTo("0x21000474657374017f")
-//    }
-//
-//    @Test
-//    fun `decode short`() {
-//        assertThat(decode(Function("test", listOf(Short(kotlin.Short.MIN_VALUE)), listOf())))
-//            .isEqualTo("0x21000474657374048000")
-//
-//        assertThat(decode(Function("test", listOf(Short(kotlin.Short.MAX_VALUE)), listOf())))
-//            .isEqualTo("0x21000474657374047fff")
-//    }
-//
-//    @Test
-//    fun `decode int`() {
-//        assertThat(decode(Function("test", listOf(Int(kotlin.Int.MIN_VALUE)), listOf())))
-//            .isEqualTo("0x210004746573740580000000")
-//
-//        assertThat(decode(Function("test", listOf(Int(kotlin.Int.MAX_VALUE)), listOf())))
-//            .isEqualTo("0x21000474657374057fffffff")
-//    }
-//
-//    @Test
-//    fun `decode long`() {
-//        assertThat(decode(Function("test", listOf(Long(kotlin.Long.MIN_VALUE)), listOf())))
-//            .isEqualTo("0x21000474657374068000000000000000")
-//
-//        assertThat(decode(Function("test", listOf(Long(kotlin.Long.MAX_VALUE)), listOf())))
-//            .isEqualTo("0x21000474657374067fffffffffffffff")
-//    }
-//
-//    @Test
-//    fun `decode float`() {
-//        assertThat(decode(Function("test", listOf(Float(kotlin.Float.MIN_VALUE)), listOf())))
-//            .isEqualTo("0x210004746573740700000001")
-//
-//        assertThat(decode(Function("test", listOf(Float(kotlin.Float.MAX_VALUE)), listOf())))
-//            .isEqualTo("0x21000474657374077f7fffff")
-//    }
-//
-//    @Test
-//    fun `decode double`() {
-//        assertThat(decode(Function("test", listOf(Double(kotlin.Double.MIN_VALUE)), listOf())))
-//            .isEqualTo("0x21000474657374080000000000000001")
-//
-//        assertThat(decode(Function("test", listOf(Double(kotlin.Double.MAX_VALUE)), listOf())))
-//            .isEqualTo("0x21000474657374087fefffffffffffff")
-//    }
-//
-//    @Test
-//    fun `decode string array`() {
-//        val empty = DynamicArray(Utf8String::class.java)
-//        assertThat(decode(Function("test", listOf(empty), listOf())))
-//            .isEqualTo("0x2100047465737431210000")
-//
-//        val single = DynamicArray(Utf8String::class.java, listOf(Utf8String("Hello AVM")))
-//        assertThat(decode(Function("test", listOf(single), listOf())))
-//            .isEqualTo("0x210004746573743121000121000948656c6c6f2041564d")
-//    }
-//
-//    @Test
-//    fun `decode 2D string array`() {
-//        assertThrows<ABIException> {
-//            val param = DynamicArray(
-//                DynamicArray::class.java,
-//                DynamicArray(Utf8String::class.java, Utf8String("Hello AVM"))
-//            )
-//            decode(Function("test", listOf(param), listOf()))
-//        }
-//    }
-//
-//    @Test
-//    fun `decode char array`() {
-//        val empty = DynamicArray(Char::class.java)
-//        assertThat(decode(Function("test", listOf(empty), listOf())))
-//            .isEqualTo("0x21000474657374130000")
-//
-//        val abc = DynamicArray(Char::class.java, listOf(Char('a'), Char('b'), Char('c')))
-//        assertThat(decode(Function("test", listOf(abc), listOf())))
-//            .isEqualTo("0x21000474657374130003006100620063")
-//    }
-//
-//    @Test
-//    fun `decode boolean array`() {
-//        val empty = DynamicArray(Boolean::class.java)
-//        assertThat(decode(Function("test", listOf(empty), listOf())))
-//            .isEqualTo("0x21000474657374120000")
-//
-//        val trueFalse = DynamicArray(Boolean::class.java, listOf(Boolean(true), Boolean(false)))
-//        assertThat(decode(Function("test", listOf(trueFalse), listOf())))
-//            .isEqualTo("0x210004746573741200020100")
-//    }
-//
-//    @Test
-//    fun `decode byte array`() {
-//        val empty = DynamicArray(Byte::class.java)
-//        assertThat(decode(Function("test", listOf(empty), listOf())))
-//            .isEqualTo("0x21000474657374110000")
-//
-//        val minMax = DynamicArray(Byte::class.java, listOf(Byte(kotlin.Byte.MIN_VALUE), Byte(kotlin.Byte.MAX_VALUE)))
-//        assertThat(decode(Function("test", listOf(minMax), listOf())))
-//            .isEqualTo("0x21000474657374110002807f")
-//    }
-//
-//    @Test
-//    fun `decode short array`() {
-//        val empty = DynamicArray(Short::class.java)
-//        assertThat(decode(Function("test", listOf(empty), listOf())))
-//            .isEqualTo("0x21000474657374140000")
-//
-//        val minMax =
-//            DynamicArray(Short::class.java, listOf(Short(kotlin.Short.MIN_VALUE), Short(kotlin.Short.MAX_VALUE)))
-//        assertThat(decode(Function("test", listOf(minMax), listOf())))
-//            .isEqualTo("0x2100047465737414000280007fff")
-//    }
-//
-//    @Test
-//    fun `decode int array`() {
-//        val empty = DynamicArray(Int::class.java)
-//        assertThat(decode(Function("test", listOf(empty), listOf())))
-//            .isEqualTo("0x21000474657374150000")
-//
-//        val minMax = DynamicArray(Int::class.java, listOf(Int(kotlin.Int.MIN_VALUE), Int(kotlin.Int.MAX_VALUE)))
-//        assertThat(decode(Function("test", listOf(minMax), listOf())))
-//            .isEqualTo("0x21000474657374150002800000007fffffff")
-//    }
-//
-//    @Test
-//    fun `decode long array`() {
-//        val empty = DynamicArray(Long::class.java)
-//        assertThat(decode(Function("test", listOf(empty), listOf())))
-//            .isEqualTo("0x21000474657374160000")
-//
-//        val minMax = DynamicArray(Long::class.java, listOf(Long(kotlin.Long.MIN_VALUE), Long(kotlin.Long.MAX_VALUE)))
-//        assertThat(decode(Function("test", listOf(minMax), listOf())))
-//            .isEqualTo("0x2100047465737416000280000000000000007fffffffffffffff")
-//    }
-//
-//    @Test
-//    fun `decode float array`() {
-//        val empty = DynamicArray(Float::class.java)
-//        assertThat(decode(Function("test", listOf(empty), listOf())))
-//            .isEqualTo("0x21000474657374170000")
-//
-//        val minMax = DynamicArray(
-//            Float::class.java,
-//            listOf(
-//                Float(kotlin.Float.MIN_VALUE),
-//                Float(kotlin.Float.MAX_VALUE)
-//            )
-//        )
-//        assertThat(decode(Function("test", listOf(minMax), listOf())))
-//            .isEqualTo("0x21000474657374170002000000017f7fffff")
-//    }
-//
-//    @Test
-//    fun `decode double array`() {
-//        val empty = DynamicArray(Double::class.java)
-//        assertThat(decode(Function("test", listOf(empty), listOf())))
-//            .isEqualTo("0x21000474657374180000")
-//
-//        val minMax = DynamicArray(
-//            Double::class.java, listOf(
-//                Double(kotlin.Double.MIN_VALUE),
-//                Double(kotlin.Double.MAX_VALUE)
-//            )
-//        )
-//        assertThat(decode(Function("test", listOf(minMax), listOf())))
-//            .isEqualTo("0x2100047465737418000200000000000000017fefffffffffffff")
-//    }
+
+    @Test
+    fun `decode byte`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<Byte>() {}))
+
+        assertThat(decode("0x210004746573740180", function.outputParameters))
+            .isEqualTo(listOf(Byte(kotlin.Byte.MIN_VALUE)))
+
+        assertThat(decode("0x21000474657374017f", function.outputParameters))
+            .isEqualTo(listOf(Byte(kotlin.Byte.MAX_VALUE)))
+    }
+
+    @Test
+    fun `decode short`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<Short>() {}))
+
+        assertThat(decode("0x21000474657374048000", function.outputParameters))
+            .isEqualTo(listOf(Short(kotlin.Short.MIN_VALUE)))
+
+        assertThat(decode("0x21000474657374047fff", function.outputParameters))
+            .isEqualTo(listOf(Short(kotlin.Short.MAX_VALUE)))
+    }
+
+    @Test
+    fun `decode int`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<Int>() {}))
+
+        assertThat(decode("0x210004746573740580000000", function.outputParameters))
+            .isEqualTo(listOf(Int(kotlin.Int.MIN_VALUE)))
+
+        assertThat(decode("0x21000474657374057fffffff", function.outputParameters))
+            .isEqualTo(listOf(Int(kotlin.Int.MAX_VALUE)))
+    }
+
+    @Test
+    fun `decode long`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<Long>() {}))
+
+        assertThat(decode("0x21000474657374068000000000000000", function.outputParameters))
+            .isEqualTo(listOf(Long(kotlin.Long.MIN_VALUE)))
+
+        assertThat(decode("0x21000474657374067fffffffffffffff", function.outputParameters))
+            .isEqualTo(listOf(Long(kotlin.Long.MAX_VALUE)))
+    }
+
+    @Test
+    fun `decode float`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<Float>() {}))
+
+        assertThat(decode("0x210004746573740700000001", function.outputParameters))
+            .isEqualTo(listOf(Float(kotlin.Float.MIN_VALUE)))
+
+        assertThat(decode("0x21000474657374077f7fffff", function.outputParameters))
+            .isEqualTo(listOf(Float(kotlin.Float.MAX_VALUE)))
+    }
+
+    @Test
+    fun `decode double`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<Double>() {}))
+
+        assertThat(decode("0x21000474657374080000000000000001", function.outputParameters))
+            .isEqualTo(listOf(Double(kotlin.Double.MIN_VALUE)))
+
+        assertThat(decode("0x21000474657374087fefffffffffffff", function.outputParameters))
+            .isEqualTo(listOf(Double(kotlin.Double.MAX_VALUE)))
+    }
+
+    @Test
+    fun `decode char array`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Char>>() {}))
+
+        assertThat(decode("0x21000474657374130000", function.outputParameters))
+            .isEqualTo(listOf(DynamicArray(Char::class.java)))
+
+        assertThat(decode("0x21000474657374130003006100620063", function.outputParameters)).isEqualTo(
+            listOf(
+                DynamicArray(
+                    Char::class.java,
+                    listOf(Char('a'), Char('b'), Char('c'))
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `decode byte array`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Byte>>() {}))
+
+        assertThat(decode("0x21000474657374110000", function.outputParameters))
+            .isEqualTo(listOf(DynamicArray(Byte::class.java)))
+
+        assertThat(decode("0x21000474657374110002807f", function.outputParameters)).isEqualTo(
+            listOf(
+                DynamicArray(
+                    Byte::class.java, listOf(
+                        Byte(kotlin.Byte.MIN_VALUE),
+                        Byte(kotlin.Byte.MAX_VALUE)
+                    )
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `decode short array`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Short>>() {}))
+
+        assertThat(decode("0x21000474657374140000", function.outputParameters))
+            .isEqualTo(listOf(DynamicArray(Short::class.java)))
+
+        assertThat(decode("0x2100047465737414000280007fff", function.outputParameters)).isEqualTo(
+            listOf(
+                DynamicArray(
+                    Short::class.java,
+                    listOf(Short(kotlin.Short.MIN_VALUE), Short(kotlin.Short.MAX_VALUE))
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `decode int array`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Int>>() {}))
+
+        assertThat(decode("0x21000474657374150000", function.outputParameters))
+            .isEqualTo(listOf(DynamicArray(Int::class.java)))
+
+        assertThat(decode("0x21000474657374150002800000007fffffff", function.outputParameters)).isEqualTo(
+            listOf(
+                DynamicArray(
+                    Int::class.java,
+                    listOf(Int(kotlin.Int.MIN_VALUE), Int(kotlin.Int.MAX_VALUE))
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `decode long array`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Long>>() {}))
+
+        assertThat(decode("0x21000474657374160000", function.outputParameters))
+            .isEqualTo(listOf(DynamicArray(Long::class.java)))
+
+        assertThat(
+            decode(
+                "0x2100047465737416000280000000000000007fffffffffffffff",
+                function.outputParameters
+            )
+        ).isEqualTo(
+            listOf(
+                DynamicArray(
+                    Long::class.java,
+                    listOf(Long(kotlin.Long.MIN_VALUE), Long(kotlin.Long.MAX_VALUE))
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `decode float array`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Float>>() {}))
+
+        assertThat(decode("0x21000474657374170000", function.outputParameters))
+            .isEqualTo(listOf(DynamicArray(Float::class.java)))
+
+        assertThat(decode("0x21000474657374170002000000017f7fffff", function.outputParameters)).isEqualTo(
+            listOf(
+                DynamicArray(
+                    Float::class.java,
+                    listOf(Float(kotlin.Float.MIN_VALUE), Float(kotlin.Float.MAX_VALUE))
+                )
+            )
+        )
+    }
+
+    @Test
+    fun `decode double array`() {
+        val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Double>>() {}))
+
+        assertThat(decode("0x21000474657374180000", function.outputParameters))
+            .isEqualTo(listOf(DynamicArray(Double::class.java)))
+
+        assertThat(
+            decode(
+                "0x2100047465737418000200000000000000017fefffffffffffff",
+                function.outputParameters
+            )
+        ).isEqualTo(
+            listOf(
+                DynamicArray(
+                    Double::class.java,
+                    listOf(Double(kotlin.Double.MIN_VALUE), Double(kotlin.Double.MAX_VALUE))
+                )
+            )
+        )
+    }
 }
