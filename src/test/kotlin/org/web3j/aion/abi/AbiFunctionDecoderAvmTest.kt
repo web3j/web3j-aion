3d79fef85ef2f3696d6f0239dd235c071bad2d6b
Xavier <xavier@blk.io>
2019-06-05 16:30:19 +0100

    Avoid decoding method name for AVM types

--- src/test/kotlin/org/web3j/aion/abi/AbiFunctionDecoderAvmTest.kt
+++ src/test/kotlin/org/web3j/aion/abi/AbiFunctionDecoderAvmTest.kt
@@ -23,7 +23,7 @@ class AbiFunctionDecoderAvmTest {
     @Test
     fun `decode char`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<Char>() {}))
-        assertThat(decode("0x21000474657374030061", function.outputParameters))
+        assertThat(decode("0x030061", function.outputParameters))
             .isEqualTo(listOf(Char('a')))
     }
 
@@ -31,10 +31,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode byte`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<Byte>() {}))
 
-        assertThat(decode("0x210004746573740180", function.outputParameters))
+        assertThat(decode("0x0180", function.outputParameters))
             .isEqualTo(listOf(Byte(kotlin.Byte.MIN_VALUE)))
 
-        assertThat(decode("0x21000474657374017f", function.outputParameters))
+        assertThat(decode("0x017f", function.outputParameters))
             .isEqualTo(listOf(Byte(kotlin.Byte.MAX_VALUE)))
     }
 
@@ -42,10 +42,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode short`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<Short>() {}))
 
-        assertThat(decode("0x21000474657374048000", function.outputParameters))
+        assertThat(decode("0x048000", function.outputParameters))
             .isEqualTo(listOf(Short(kotlin.Short.MIN_VALUE)))
 
-        assertThat(decode("0x21000474657374047fff", function.outputParameters))
+        assertThat(decode("0x047fff", function.outputParameters))
             .isEqualTo(listOf(Short(kotlin.Short.MAX_VALUE)))
     }
 
@@ -53,10 +53,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode int`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<Int>() {}))
 
-        assertThat(decode("0x210004746573740580000000", function.outputParameters))
+        assertThat(decode("0x0580000000", function.outputParameters))
             .isEqualTo(listOf(Int(kotlin.Int.MIN_VALUE)))
 
-        assertThat(decode("0x21000474657374057fffffff", function.outputParameters))
+        assertThat(decode("0x057fffffff", function.outputParameters))
             .isEqualTo(listOf(Int(kotlin.Int.MAX_VALUE)))
     }
 
@@ -64,10 +64,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode long`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<Long>() {}))
 
-        assertThat(decode("0x21000474657374068000000000000000", function.outputParameters))
+        assertThat(decode("0x068000000000000000", function.outputParameters))
             .isEqualTo(listOf(Long(kotlin.Long.MIN_VALUE)))
 
-        assertThat(decode("0x21000474657374067fffffffffffffff", function.outputParameters))
+        assertThat(decode("0x067fffffffffffffff", function.outputParameters))
             .isEqualTo(listOf(Long(kotlin.Long.MAX_VALUE)))
     }
 
@@ -75,10 +75,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode float`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<Float>() {}))
 
-        assertThat(decode("0x210004746573740700000001", function.outputParameters))
+        assertThat(decode("0x0700000001", function.outputParameters))
             .isEqualTo(listOf(Float(kotlin.Float.MIN_VALUE)))
 
-        assertThat(decode("0x21000474657374077f7fffff", function.outputParameters))
+        assertThat(decode("0x077f7fffff", function.outputParameters))
             .isEqualTo(listOf(Float(kotlin.Float.MAX_VALUE)))
     }
 
@@ -86,10 +86,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode double`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<Double>() {}))
 
-        assertThat(decode("0x21000474657374080000000000000001", function.outputParameters))
+        assertThat(decode("0x080000000000000001", function.outputParameters))
             .isEqualTo(listOf(Double(kotlin.Double.MIN_VALUE)))
 
-        assertThat(decode("0x21000474657374087fefffffffffffff", function.outputParameters))
+        assertThat(decode("0x087fefffffffffffff", function.outputParameters))
             .isEqualTo(listOf(Double(kotlin.Double.MAX_VALUE)))
     }
 
@@ -97,10 +97,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode char array`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Char>>() {}))
 
-        assertThat(decode("0x21000474657374130000", function.outputParameters))
+        assertThat(decode("0x130000", function.outputParameters))
             .isEqualTo(listOf(DynamicArray(Char::class.java)))
 
-        assertThat(decode("0x21000474657374130003006100620063", function.outputParameters)).isEqualTo(
+        assertThat(decode("0x130003006100620063", function.outputParameters)).isEqualTo(
             listOf(
                 DynamicArray(
                     Char::class.java,
@@ -114,10 +114,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode byte array`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Byte>>() {}))
 
-        assertThat(decode("0x21000474657374110000", function.outputParameters))
+        assertThat(decode("0x110000", function.outputParameters))
             .isEqualTo(listOf(DynamicArray(Byte::class.java)))
 
-        assertThat(decode("0x21000474657374110002807f", function.outputParameters)).isEqualTo(
+        assertThat(decode("0x110002807f", function.outputParameters)).isEqualTo(
             listOf(
                 DynamicArray(
                     Byte::class.java, listOf(
@@ -133,10 +133,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode short array`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Short>>() {}))
 
-        assertThat(decode("0x21000474657374140000", function.outputParameters))
+        assertThat(decode("0x140000", function.outputParameters))
             .isEqualTo(listOf(DynamicArray(Short::class.java)))
 
-        assertThat(decode("0x2100047465737414000280007fff", function.outputParameters)).isEqualTo(
+        assertThat(decode("0x14000280007fff", function.outputParameters)).isEqualTo(
             listOf(
                 DynamicArray(
                     Short::class.java,
@@ -150,10 +150,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode int array`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Int>>() {}))
 
-        assertThat(decode("0x21000474657374150000", function.outputParameters))
+        assertThat(decode("0x150000", function.outputParameters))
             .isEqualTo(listOf(DynamicArray(Int::class.java)))
 
-        assertThat(decode("0x21000474657374150002800000007fffffff", function.outputParameters)).isEqualTo(
+        assertThat(decode("0x150002800000007fffffff", function.outputParameters)).isEqualTo(
             listOf(
                 DynamicArray(
                     Int::class.java,
@@ -167,12 +167,12 @@ class AbiFunctionDecoderAvmTest {
     fun `decode long array`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Long>>() {}))
 
-        assertThat(decode("0x21000474657374160000", function.outputParameters))
+        assertThat(decode("0x160000", function.outputParameters))
             .isEqualTo(listOf(DynamicArray(Long::class.java)))
 
         assertThat(
             decode(
-                "0x2100047465737416000280000000000000007fffffffffffffff",
+                "0x16000280000000000000007fffffffffffffff",
                 function.outputParameters
             )
         ).isEqualTo(
@@ -189,10 +189,10 @@ class AbiFunctionDecoderAvmTest {
     fun `decode float array`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Float>>() {}))
 
-        assertThat(decode("0x21000474657374170000", function.outputParameters))
+        assertThat(decode("0x170000", function.outputParameters))
             .isEqualTo(listOf(DynamicArray(Float::class.java)))
 
-        assertThat(decode("0x21000474657374170002000000017f7fffff", function.outputParameters)).isEqualTo(
+        assertThat(decode("0x170002000000017f7fffff", function.outputParameters)).isEqualTo(
             listOf(
                 DynamicArray(
                     Float::class.java,
@@ -206,12 +206,12 @@ class AbiFunctionDecoderAvmTest {
     fun `decode double array`() {
         val function = Function("test", listOf(), listOf(object : TypeReference<DynamicArray<Double>>() {}))
 
-        assertThat(decode("0x21000474657374180000", function.outputParameters))
+        assertThat(decode("0x180000", function.outputParameters))
             .isEqualTo(listOf(DynamicArray(Double::class.java)))
 
         assertThat(
             decode(
-                "0x2100047465737418000200000000000000017fefffffffffffff",
+                "0x18000200000000000000017fefffffffffffff",
                 function.outputParameters
             )
         ).isEqualTo(
