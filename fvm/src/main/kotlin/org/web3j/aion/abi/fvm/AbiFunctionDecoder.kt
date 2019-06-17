6ac56041eabaac7cf0b234af1b61cff2aa26ad12
Xavier <xavier@blk.io>
2019-06-17 17:31:20 +0100

    Remove unneeded comments

--- fvm/src/main/kotlin/org/web3j/aion/abi/fvm/AbiFunctionDecoder.kt
+++ fvm/src/main/kotlin/org/web3j/aion/abi/fvm/AbiFunctionDecoder.kt
@@ -1,6 +1,5 @@
 package org.web3j.aion.abi.fvm
 
-// FIXME Remove core dependency
 import org.web3j.abi.DefaultFunctionReturnDecoder
 
 internal object AbiFunctionDecoder : DefaultFunctionReturnDecoder()
--- fvm/src/main/kotlin/org/web3j/aion/abi/fvm/AbiFunctionEncoder.kt
+++ fvm/src/main/kotlin/org/web3j/aion/abi/fvm/AbiFunctionEncoder.kt
@@ -1,6 +1,5 @@
 package org.web3j.aion.abi.fvm
 
-// FIXME Remove core dependency
 import org.web3j.abi.DefaultFunctionEncoder
 
 internal object AbiFunctionEncoder : DefaultFunctionEncoder()
