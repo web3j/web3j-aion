b91a4cd5658f01df26a0cedb6d36a322e7845ae3
Xavier <xavier@blk.io>
2019-06-18 15:20:07 +0100

    Refactor IT base class to allow target VM in tx manager

--- common/src/integration-test/kotlin/org/web3j/aion/protocol/AionIntegrationTest.kt
+++ common/src/integration-test/kotlin/org/web3j/aion/protocol/AionIntegrationTest.kt
@@ -1,6 +1,11 @@
 package org.web3j.aion.protocol
 
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.testcontainers.containers.BindMode.READ_ONLY
+import org.testcontainers.containers.BindMode.READ_WRITE
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import org.web3j.aion.VirtualMachine
 import org.web3j.aion.crypto.Ed25519KeyPair
 import org.web3j.aion.protocol.AionIntegrationTest.Network.LOCALHOST
@@ -9,24 +14,46 @@ import org.web3j.aion.tx.AionTransactionManager
 import org.web3j.protocol.http.HttpService
 import org.web3j.tx.TransactionManager
 
-// @Testcontainers
+@Testcontainers
 abstract class AionIntegrationTest(private val targetVm: VirtualMachine) {
+
+    protected lateinit var manager: TransactionManager
 
     enum class Network {
         LOCALHOST,
         MASTERY
     }
 
+    @BeforeEach
+    fun setUp() {
+        manager = AionTransactionManager(aion, ACCOUNT.getValue(NETWORK), keyPair, targetVm)
+
+        if (NETWORK != MASTERY) { // Unlock account not supported in Nodesmith
+            aion.personalUnlockAccount(ACCOUNT[NETWORK], "410n").send()
+        }
+    }
+
     companion object {
 
+        @Container
         @JvmStatic
-        protected val NETWORK = MASTERY
+        private val AION = KGenericContainer("aionnetwork/aion:0.4.0.1")
+            .withClasspathResourceMapping("aion/config", "/aion/custom/config", READ_ONLY)
+            .withClasspathResourceMapping("aion/keystore", "/aion/custom/keystore", READ_ONLY)
+            .withClasspathResourceMapping("aion/log", "/aion/custom/log", READ_WRITE)
+            .withCommand("/aion/aion.sh --network custom")
+            .withExposedPorts(8545)
 
-        private val RPC_URL = mapOf(
-            LOCALHOST to "http://localhost:8545",
-//            LOCALHOST to "http://${AION.containerIpAddress}:${AION.getMappedPort(8545)}/",
-            MASTERY to "https://aion.api.nodesmith.io/v1/mastery/jsonrpc?apiKey=7b1d449f07774200bc1000a8b0eb1a9e"
-        )
+        @JvmStatic
+        protected val NETWORK = LOCALHOST
+
+        @JvmStatic
+        private val RPC_URL: Map<Network, String> by lazy {
+            mapOf(
+                LOCALHOST to "http://${AION.containerIpAddress}:${AION.getMappedPort(8545)}/",
+                MASTERY to "https://aion.api.nodesmith.io/v1/mastery/jsonrpc?apiKey=7b1d449f07774200bc1000a8b0eb1a9e"
+            )
+        }
 
         @JvmStatic
         protected val ACCOUNT = mapOf(
@@ -47,33 +74,15 @@ abstract class AionIntegrationTest(private val targetVm: VirtualMachine) {
         )
 
         @JvmStatic
-        protected lateinit var aion: Aion
-
-        @JvmStatic
-        protected lateinit var manager: TransactionManager
-
-        @BeforeAll
-        @JvmStatic
-        fun initClient() {
-            aion = Aion.build(HttpService(RPC_URL[NETWORK]))
-
-            val keyPair = Ed25519KeyPair(PUBLIC_KEY.getValue(NETWORK), PRIVATE_KEY.getValue(NETWORK))
-            manager = AionTransactionManager(aion, ACCOUNT.getValue(NETWORK), keyPair, VirtualMachine.AVM)
-
-            if (NETWORK != MASTERY) { // Unlock account not supported in Nodesmith
-                aion.personalUnlockAccount(ACCOUNT[NETWORK], "410n").send()
-            }
+        protected val aion: Aion by lazy {
+            Aion.build(HttpService(RPC_URL[NETWORK]))
         }
 
-//        @Container
-//        @JvmStatic
-//        private val AION = KGenericContainer("aionnetwork/aion:0.4.0.1")
-//            .withClasspathResourceMapping("aion/config", "/aion/custom/config", BindMode.READ_ONLY)
-//            .withClasspathResourceMapping("aion/keystore", "/aion/custom/keystore", BindMode.READ_ONLY)
-//            .withClasspathResourceMapping("aion/log", "/aion/custom/log", BindMode.READ_WRITE)
-//            .withCommand("/aion/aion.sh --network custom")
-//            .withExposedPorts(8545)
+        @JvmStatic
+        private val keyPair: Ed25519KeyPair by lazy {
+            Ed25519KeyPair(PUBLIC_KEY.getValue(NETWORK), PRIVATE_KEY.getValue(NETWORK))
+        }
 
-//        class KGenericContainer(imageName: String) : GenericContainer<KGenericContainer>(imageName)
+        class KGenericContainer(imageName: String) : GenericContainer<KGenericContainer>(imageName)
     }
 }
