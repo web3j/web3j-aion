0695479d7da84dd5331ef2c6e3a3d9f33d300b6c
Xavier <xavier@blk.io>
2019-06-24 10:01:51 +0100

    WIP FVM Encoder

--- common/src/main/kotlin/org/web3j/aion/abi/AionAbiException.kt
+++ common/src/main/kotlin/org/web3j/aion/abi/AionAbiException.kt
@@ -1,7 +1,11 @@
 package org.web3j.aion.abi
 
+import org.web3j.abi.datatypes.Type
+
 sealed class AionAbiException(message: String, cause: Throwable? = null) : Exception(message, cause)
 
 class AionEncoderException(message: String, cause: Throwable? = null) : AionAbiException(message, cause)
-
 class AionDecoderException(message: String, cause: Throwable? = null) : AionAbiException(message, cause)
+
+class UnsupportedTypeException(value: Type<*>) : AionAbiException("Unsupported type ${value.typeAsString}")
+class UnknownTypeException(value: Type<*>) : AionAbiException("Unknown type ${value.typeAsString}")
--- fvm/src/main/kotlin/org/web3j/aion/abi/fvm/AbiFunctionEncoder.kt
+++ fvm/src/main/kotlin/org/web3j/aion/abi/fvm/AbiFunctionEncoder.kt
@@ -1,5 +1,74 @@
 package org.web3j.aion.abi.fvm
 
 import org.web3j.abi.DefaultFunctionEncoder
+import org.web3j.abi.TypeReference
+import org.web3j.abi.datatypes.Array
+import org.web3j.abi.datatypes.DynamicArray
+import org.web3j.abi.datatypes.FixedPointType
+import org.web3j.abi.datatypes.Function
+import org.web3j.abi.datatypes.Int
+import org.web3j.abi.datatypes.IntType
+import org.web3j.abi.datatypes.Type
+import org.web3j.abi.datatypes.Uint
+import org.web3j.aion.abi.AionEncoderException
+import org.web3j.aion.abi.UnknownTypeException
+import org.web3j.aion.abi.UnsupportedTypeException
 
-internal object AbiFunctionEncoder : DefaultFunctionEncoder()
+internal object AbiFunctionEncoder : DefaultFunctionEncoder() {
+
+    override fun encodeFunction(function: Function): String {
+        val params = function.inputParameters.map { it.toAion() }
+        return super.encodeFunction(
+            Function(
+                function.name, params,
+                function.outputParameters as List<TypeReference<*>>?
+            )
+        )
+    }
+
+    override fun encodeParameters(parameters: List<Type<Any>>): String {
+        val params = parameters.map { it.toAion() }
+        return super.encodeParameters(params)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> Type<T>.toAion(): Type<*> {
+        return when (this) {
+            is FixedPointType -> throw AionEncoderException("Unsupported fixed point type")
+            is IntType -> (this as IntType).toAion()
+            is Array<*> -> (this as Array<Type<*>>).toAion()
+            else -> this
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun Array<Type<*>>.toAion(): Array<Type<*>> {
+        return when (this.componentType) {
+            IntType::class.java -> {
+                val values = (value as List<IntType>).map { it.toAion() }
+                DynamicArray<IntType>(this.componentType as Class<IntType>, values) as Array<Type<*>>
+            }
+//                DynamicArray::class.java, StaticArray::class.java ->
+//                TODO("2D arrays not implemented")
+//                      (value as List<DynamicArray<Type<Char>>>)
+//                      .map { it.toAion() as CharArray }.toTypedArray()
+            else -> this
+        }
+    }
+
+    private fun IntType.toAion(): IntType {
+        return when (this) {
+            is Int -> when (bitSize) {
+                in 8..128 -> this
+                in 136..256 -> throw UnsupportedTypeException(this)
+                else -> throw UnknownTypeException(this)
+            }
+            is Uint -> when (bitSize) {
+                in 8..128 -> this
+                in 136..256 -> throw UnsupportedTypeException(this)
+                else -> throw UnknownTypeException(this)
+            }
+            else -> throw UnknownTypeException(this)
+        }
+    }
+}
