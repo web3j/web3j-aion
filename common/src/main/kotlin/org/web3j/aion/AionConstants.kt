c1695b2faf889523c2b189a61314b9a42411a783
Xavier <xavier@blk.io>
2019-06-25 11:10:28 +0100

    Add Aion constants and use max value for NRG to avoid finishing gas

--- common/src/main/kotlin/org/web3j/aion/AionConstants.kt
+++ common/src/main/kotlin/org/web3j/aion/AionConstants.kt
@@ -5,8 +5,26 @@ import org.web3j.utils.Numeric
 object AionConstants {
     val ADDRESS_ID: ByteArray = Numeric.hexStringToByteArray("a0")
 
+    const val NRG_CODE_DEPOSIT = 1000L
+
+    const val NRG_CREATE_CONTRACT_MIN = 200000L
+
     const val NRG_CREATE_CONTRACT_DEFAULT = 350000L
+
+    const val NRG_CREATE_CONTRACT_MAX = 5000000L
+
+    const val NRG_TX_DATA_ZERO = 4L
+
+    const val NRG_TX_DATA_NONZERO = 64L
+
+    const val NRG_TRANSACTION_MIN = 21000L
+
     const val NRG_TRANSACTION_DEFAULT = 90000L
+
+    const val NRG_TRANSACTION_MAX = 2000000L
+
+    /** Call stack depth limit. Based on EIP-150, the theoretical limit is ~340.  */
+    val MAX_CALL_DEPTH = 128
 
     const val MAX_BIT_LENGTH = 128
     const val MAX_BYTE_LENGTH = MAX_BIT_LENGTH / 8
--- common/src/main/kotlin/org/web3j/aion/tx/gas/AionGasProvider.kt
+++ common/src/main/kotlin/org/web3j/aion/tx/gas/AionGasProvider.kt
@@ -1,7 +1,7 @@
 package org.web3j.aion.tx.gas
 
-import org.web3j.aion.AionConstants.NRG_CREATE_CONTRACT_DEFAULT
-import org.web3j.aion.AionConstants.NRG_TRANSACTION_DEFAULT
+import org.web3j.aion.AionConstants.NRG_CREATE_CONTRACT_MAX
+import org.web3j.aion.AionConstants.NRG_TRANSACTION_MAX
 import org.web3j.tx.gas.StaticGasProvider
 import java.math.BigInteger
 
@@ -9,8 +9,8 @@ object AionGasProvider : StaticGasProvider(
     BigInteger.valueOf(10_000_000_000),
     BigInteger.ZERO // Overriden here
 ) {
-    private val CREATE_CONTRACT_DEFAULT = BigInteger.valueOf(NRG_CREATE_CONTRACT_DEFAULT * 10)
-    private val TRANSACTION_DEFAULT = BigInteger.valueOf(NRG_TRANSACTION_DEFAULT)
+    private val CREATE_CONTRACT_DEFAULT = BigInteger.valueOf(NRG_CREATE_CONTRACT_MAX)
+    private val TRANSACTION_DEFAULT = BigInteger.valueOf(NRG_TRANSACTION_MAX)
 
     override fun getGasLimit(): BigInteger {
         return TRANSACTION_DEFAULT
