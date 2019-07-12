ad9651d45030ae919d5bd8d0b8c1bf225d6f676f
Xavier <xavier@blk.io>
2019-07-12 19:13:21 +0100

    Move FVM cases to fvm module

--- avm/src/main/kotlin/org/web3j/aion/abi/avm/AbiFunctionDecoder.kt
+++ avm/src/main/kotlin/org/web3j/aion/abi/avm/AbiFunctionDecoder.kt
@@ -5,28 +5,9 @@ import org.web3j.abi.FunctionReturnDecoder
 import org.web3j.abi.TypeReference
 import org.web3j.abi.datatypes.Address
 import org.web3j.abi.datatypes.Bool
-import org.web3j.abi.datatypes.BytesType
 import org.web3j.abi.datatypes.DynamicArray
-import org.web3j.abi.datatypes.IntType
-import org.web3j.abi.datatypes.StaticArray
 import org.web3j.abi.datatypes.Type
-import org.web3j.abi.datatypes.Uint
 import org.web3j.abi.datatypes.Utf8String
-import org.web3j.abi.datatypes.generated.Int16
-import org.web3j.abi.datatypes.generated.Int24
-import org.web3j.abi.datatypes.generated.Int32
-import org.web3j.abi.datatypes.generated.Int40
-import org.web3j.abi.datatypes.generated.Int48
-import org.web3j.abi.datatypes.generated.Int56
-import org.web3j.abi.datatypes.generated.Int64
-import org.web3j.abi.datatypes.generated.Int8
-import org.web3j.abi.datatypes.generated.Uint16
-import org.web3j.abi.datatypes.generated.Uint24
-import org.web3j.abi.datatypes.generated.Uint32
-import org.web3j.abi.datatypes.generated.Uint40
-import org.web3j.abi.datatypes.generated.Uint48
-import org.web3j.abi.datatypes.generated.Uint56
-import org.web3j.abi.datatypes.generated.Uint8
 import org.web3j.abi.datatypes.primitive.Byte
 import org.web3j.abi.datatypes.primitive.Char
 import org.web3j.abi.datatypes.primitive.Double
@@ -35,27 +16,11 @@ import org.web3j.abi.datatypes.primitive.Int
 import org.web3j.abi.datatypes.primitive.Long
 import org.web3j.abi.datatypes.primitive.Short
 import org.web3j.aion.AionConstants.ADDRESS_BIT_LENGTH
-import org.web3j.aion.abi.AionDecoderException
+import org.web3j.aion.abi.UnsupportedTypeException
 import org.web3j.utils.Numeric
 import java.lang.reflect.ParameterizedType
-import java.math.BigInteger
 
 internal object AbiFunctionDecoder : FunctionReturnDecoder() {
-
-    /**
-     * Regex to match the bit size of an [org.web3j.abi.datatypes.Int].
-     */
-    private val INT_REGEX = "^Int(\\d*)\$".toRegex()
-
-    /**
-     * Regex to match the bit size of an unsigned [Uint].
-     */
-    private val UINT_REGEX = "^Uint(\\d*)\$".toRegex()
-
-    /**
-     * Regex to match the bit size of a [org.web3j.abi.datatypes.Bytes].
-     */
-    private val BYTES_REGEX = "^Bytes(\\d*)\$".toRegex()
 
     override fun decodeFunctionResult(
         rawInput: String,
@@ -80,13 +45,8 @@ internal object AbiFunctionDecoder : FunctionReturnDecoder() {
                 Double::class.java.isAssignableFrom(type) -> decodeOneDouble().toWeb3j()
                 Utf8String::class.java.isAssignableFrom(type) -> decodeOneString().toWeb3j()
                 Bool::class.java.isAssignableFrom(type) -> decodeOneBoolean().toWeb3j()
-                BytesType::class.java.isAssignableFrom(type) -> TODO("FVM not implemented")
-                // decodeBytesType(type, decoder)
-                IntType::class.java.isAssignableFrom(type) -> decodeIntType(type, decoder)
                 DynamicArray::class.java.isAssignableFrom(type) -> decodeDynamicArray(typeReference, decoder)
-                StaticArray::class.java.isAssignableFrom(type) -> TODO("FVM not implemented")
-                // decodeStaticArray(typeReference, decoder)
-                else -> throw AionDecoderException("Unsupported ABI type")
+                else -> throw UnsupportedTypeException(type)
             }
         }
     }
@@ -95,64 +55,8 @@ internal object AbiFunctionDecoder : FunctionReturnDecoder() {
         rawInput: String,
         typeReference: TypeReference<T>
     ): Type<*> {
-        TODO("Solidity events not implemented")
+        TODO("AVM events not implemented")
     }
-
-    private fun decodeIntType(type: Class<Type<*>>, decoder: ABIDecoder): IntType {
-        return when {
-            org.web3j.abi.datatypes.Int::class.java.isAssignableFrom(type) ->
-                when (type.bitSize) {
-                    8 -> Int8(decoder.decodeOneByte().toLong())
-                    16 -> Int16(decoder.decodeOneShort().toLong())
-                    24 -> Int24(decoder.decodeOneInteger().toLong())
-                    32 -> Int32(decoder.decodeOneInteger().toLong())
-                    40 -> Int40(decoder.decodeOneLong())
-                    48 -> Int48(decoder.decodeOneLong())
-                    56 -> Int56(decoder.decodeOneLong())
-                    64 -> Int64(decoder.decodeOneLong())
-                    else -> newNumericInstance(type.bitSize, decoder, false)
-                }
-            Uint::class.java.isAssignableFrom(type) ->
-                when (type.bitSize) {
-                    8 -> Uint8(decoder.decodeOneShort().toLong())
-                    16 -> Uint16(decoder.decodeOneInteger().toLong())
-                    24 -> Uint24(decoder.decodeOneInteger().toLong())
-                    32 -> Uint32(decoder.decodeOneLong())
-                    40 -> Uint40(decoder.decodeOneLong())
-                    48 -> Uint48(decoder.decodeOneLong())
-                    56 -> Uint56(decoder.decodeOneLong())
-                    else -> newNumericInstance(type.bitSize, decoder, true)
-                }
-            else -> throw AionDecoderException("Unknown type ${javaClass.canonicalName}")
-        }
-    }
-
-    private val Class<Type<*>>.bitSize: kotlin.Int?
-        get() = when {
-            INT_REGEX.matches(simpleName) -> bitSize(simpleName, INT_REGEX)
-            UINT_REGEX.matches(simpleName) -> bitSize(simpleName, UINT_REGEX)
-            BYTES_REGEX.matches(simpleName) -> bitSize(simpleName, BYTES_REGEX)
-            else -> throw AionDecoderException("Unknown type ${javaClass.canonicalName}")
-        }
-
-    private fun bitSize(input: String, regex: Regex): kotlin.Int? {
-        return regex.matchEntire(input)?.groupValues?.get(1)?.toIntOrNull()
-    }
-
-    private fun newNumericInstance(bitSize: kotlin.Int?, decoder: ABIDecoder, unsigned: Boolean): IntType {
-
-        val className = "${if (unsigned) "Uint" else "Int"}${bitSize ?: ""}"
-        val packageName = "org.web3j.abi.datatypes${if (bitSize != null) ".generated" else ""}"
-
-        val constructor = Class.forName("$packageName.$className")
-            .getDeclaredConstructor(BigInteger::class.java)
-
-        return constructor.newInstance(BigInteger(decoder.decodeOneByteArray())) as IntType
-    }
-
-//    private fun decodeBytesType(type: Class<Type<*>>, decoder: ABIDecoder): BytesType {
-//        TODO("not implemented")
-//    }
 
     @Suppress("UNCHECKED_CAST")
     private fun decodeDynamicArray(typeReference: TypeReference<*>, decoder: ABIDecoder): DynamicArray<Type<*>> {
@@ -190,11 +94,8 @@ internal object AbiFunctionDecoder : FunctionReturnDecoder() {
                 Double::class.java -> DynamicArray(
                     Double::class.java,
                     decodeOneDoubleArray().map { it.toWeb3j() } as List<Double>)
-                BytesType::class.java -> TODO()
-                IntType::class.java -> TODO()
                 DynamicArray::class.java -> TODO("2D arrays not implemented")
-                StaticArray::class.java -> TODO("2D arrays not implemented")
-                else -> throw AionDecoderException("Unsupported ABI type")
+                else -> throw UnsupportedTypeException(type)
             } as DynamicArray<Type<*>>
         }
     }
@@ -216,7 +117,7 @@ internal object AbiFunctionDecoder : FunctionReturnDecoder() {
             is kotlin.Long -> Long(this)
             is kotlin.Double -> Double(this)
             is String -> Utf8String(this)
-            else -> throw AionDecoderException("Unsupported ABI type")
+            else -> throw UnsupportedTypeException(javaClass)
         }
     }
 
